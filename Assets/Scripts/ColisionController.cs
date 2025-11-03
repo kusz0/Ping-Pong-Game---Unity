7ab728232b72020ee369835746d39bdc8a529891
Kacper Kuszek <kuszekkacper@gmail.com>
2025-11-03 18:39:21 +0100

    Added ball colison controller

---
+++ Assets/Scripts/ColisionController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class ColisionController : MonoBehaviour
+{
+    public BallMovement ballMovement;
+
+    void BounceFromRacet(Collision2D c2D)
+    {
+        Vector3 ballPosition = transform.position;
+
+        Vector3 racetPosition = c2D.gameObject.transform.position;
+
+        float racetHight = c2D.collider.bounds.size.y;
+
+        float x;
+        if(c2D.gameObject.name == "LeftRacet")
+        {
+            x = 1;
+        }else
+        {
+            x = -1;
+        }
+
+        float y = (ballPosition.y - racetPosition.y) / racetHight;
+
+        ballMovement.IncreaseHitCounter();
+        ballMovement.MoveBall(new Vector2 (x, y));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "LeftRacet" || collision.gameObject.name == "RightRacet")
+        {
+            BounceFromRacet(collision);
+        }else if(collision.gameObject.name == "LeftWall" )
+        {
+            Debug.Log("Collison with LeftWall");
+        }
+        else if (collision.gameObject.name == "RightWall")
+        {
+            Debug.Log("Collison with RightWall");
+        }
+    }
+
+}
